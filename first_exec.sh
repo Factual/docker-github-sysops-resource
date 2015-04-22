f09dae9698c62cc900c5dd1c8bd092d4c67cd6c8
Maxime Devalland <maxime@factual.com>
2015-04-22 15:32:06 -0700

    switched to non-ssl configuration, now ssl is optional

--- first_exec.sh
+++ first_exec.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
+
 DOMAIN=${DOMAIN:-localhost}
+PORT=${PORT:-80}
 mkdir -p /etc/ssl/nginx
 
-echo "$SSL_CERT" > /etc/ssl/nginx/server.crt
-echo "$SSL_KEY" > /etc/ssl/nginx/server.key
-echo "$SSL_CLIENT" > /etc/ssl/nginx/client.pem
+if [[ "$PORT" == 443 ]]; then
+
+	if [[ ! -z $SSL_CERT ]] &&  [[ ! -z $SSL_KEY ]] && [[ ! -z $SSL_CLIENT ]]; then
+
+		echo "$SSL_CERT" > /etc/ssl/nginx/server.crt
+		echo "$SSL_KEY" > /etc/ssl/nginx/server.key
+		echo "$SSL_CLIENT" > /etc/ssl/nginx/client.pem
+		sed -i "s/listen 80/listen $PORT/" /etc/nginx/sites-available/default
+	else
+		echo "One or more SSL environment variables are missing : SSL_CERT, SSL_KEY, SSL_CLIENT"
+	fi
+fi
 
 sed -i "s/server_name.*/server_name $DOMAIN/" /etc/nginx/sites-available/default
